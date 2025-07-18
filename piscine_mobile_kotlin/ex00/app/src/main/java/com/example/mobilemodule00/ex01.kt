919b1198ca6a06ff1d0382296f8ee8cd03f3b867
Maxime Briand <pro.maximebriand@gmail.com>
2025-07-19 01:39:55 +0200

    ex00 done

---
+++ piscine_mobile_kotlin/ex00/app/src/main/java/com/example/mobilemodule00/ex01.kt
@@ -0,0 +1,76 @@
+package com.example.mobilemodule01
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobilemodule00.ui.theme.MobileModule00Theme
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MobileModule00Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DisplayTextAndButton(
+                        name = "Android",
+                        modifier = Modifier.padding(16.dp),
+                        from = "Barnabe",
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DisplayTextAndButton(name: String, modifier: Modifier = Modifier, from: String) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "Meraba Ed!",
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.fillMaxWidth(1f),
+            textAlign = TextAlign.Center,
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+        Button(
+            onClick = { Log.d("App", "Button pressed") },
+            modifier = Modifier.size(width = 120.dp, height = 40.dp)
+        ) { Text("Click me", style = MaterialTheme.typography.bodySmall) }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DisplayTextAndButtonPreview() {
+    MobileModule00Theme {
+        DisplayTextAndButton("Android", from = "jdjd")
+    }
+}
+
